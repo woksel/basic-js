e4d5cbe64569e269d8fdfd5efeaa42369e066b13
woksel <56549819+woksel@users.noreply.github.com>
2020-03-19 16:54:41 +0400

    feat: delete other source files

--- src/hanoi-tower.js
+++
@@ -1,8 +0,0 @@
-module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
-   let n = Math.pow(2,disksNumber)-1;
-   let sec = n/(turnsSpeed/3600);
-   return {
-    turns: n,
-    seconds: sec
-}
-}
